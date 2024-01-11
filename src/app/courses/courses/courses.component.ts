e2f483903aa6821ecdf94056ef60b0d5aa74307d
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-11 20:29:34 -0300

    feat: was create a pipe for dynamic icons on category colunm

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,25 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
 
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-//import { Observable } from 'rxjs/internal/Observable';
-import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs/internal/Observable';
-import { catchError, of } from 'rxjs';
-import { publicDecrypt } from 'crypto';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { SharedModule } from "../../shared/shared.module";
 
+//import { Observable } from 'rxjs/internal/Observable';
 @Component({
-  selector: 'app-courses',
-  standalone: true,
-  imports: [
-    AppMaterialModule,
-    CommonModule,
-  ],
-  templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+    selector: 'app-courses',
+    standalone: true,
+    templateUrl: './courses.component.html',
+    styleUrl: './courses.component.scss',
+    imports: [
+        AppMaterialModule,
+        CommonModule,
+        SharedModule
+    ]
 })
 export class CoursesComponent {
   courses: Observable<Course[]>;
