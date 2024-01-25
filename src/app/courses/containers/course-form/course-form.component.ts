b5c76bac28645d06bdc3d16d38ec2e246a653aff
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-25 16:54:40 -0300

    feat: add and remove lessons for form

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -67,6 +67,16 @@ export class CourseFormComponent {
     return (<UntypedFormArray>this.form.get("lessons")).controls
   }
 
+  addNewLesson(){
+    const lessons = this.form.get("lessons") as UntypedFormArray
+    lessons.push(this.createLesson())
+  }
+
+  removeLesson(index: number){
+    const lessons = this.form.get("lessons") as UntypedFormArray
+    lessons.removeAt(index)
+  }
+
   onCancel(){
     this.location.back()
   }
