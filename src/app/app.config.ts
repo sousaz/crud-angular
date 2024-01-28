b70ff4e19bb0368209e7101b89441c5e1e86a20c
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-28 15:22:36 -0300

    build: migration routes to standalone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimations(), provideHttpClient(withFetch())]
+  providers: [provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)), provideClientHydration(), provideAnimations(), provideAnimations(), provideHttpClient(withFetch())]
 };
